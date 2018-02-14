f66b7917324c1b1a1921c94df6df2f5e35db5800
vitorsei <seiji.vitor@gmail.com>
2018-02-15 00:57:43 +1100

    Add style-components-ts

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import styled from 'styled-components';
+import styledTS from 'styled-components-ts';
 import Headline from "./headline";
 
 interface HeaderProps {
@@ -9,28 +10,31 @@ interface HeaderProps {
 }
 
 const Header = ({text, logo, backgroundImg}: HeaderProps) => {
-
-    const Wrapper = styled.header`
-      box-sizing: border-box;
-      position: relative;
-      background-image: url(${backgroundImg});
-      background-repeat:no-repeat;
-      background-color: black;
-      background-size:contain;
-      background-position:center;
-      border: 5px solid black;
-      max-width: 320px;
-      height: 180px;
-      
-      @media (max-width: 320px) {
-          max-width: 100%;
-      }
-    `;
-
     return (
-        <Wrapper>
+        <Wrapper backgroundImg={backgroundImg}>
             <Headline logo={logo} text={text}/>
         </Wrapper>
     );
 };
+
+interface MyImageProps {
+    backgroundImg: string;
+}
+
+const Wrapper = styledTS<MyImageProps>(styled.header) `
+   box-sizing: border-box;
+   position: relative;
+   background-image: url(${props => props.backgroundImg});
+   background-repeat:no-repeat;
+   background-color: black;
+   background-size:contain;
+   background-position:center;
+   border: 5px solid black;
+   max-width: 320px;
+   height: 180px;
+   @media (max-width: 320px) {
+       max-width: 100%;
+   }
+`;
+
 export default Header;
